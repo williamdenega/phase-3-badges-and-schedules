5500b0b8cb9445f7d997797b72603802eb442c4d
William Denega <williamdenega@gmail.com>
2023-10-27 12:34:27 -0400

    commit tree 77119b694d9d3879b0aeadcbc6ee43373be09352

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
+require 'pry'
+
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    batch = []
+    arr.each {|name|  batch << badge_maker(name) }
+    #badge_maker(name)
+    batch
+end
+
+def assign_rooms arr
+    room_list = []
+    arr.each_with_index {|name,index| room_list << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+    room_list
+end
+
+def printer arr
+    batch_badge_creator(arr).each {|msg| puts msg}
+    assign_rooms(arr).each {|msg| puts msg}
+
+end
+
+
+binding.pry
